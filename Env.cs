23a9cd7690d62cfeb7697078be01ea1d0b6ad6ad
p1tchbend-tool <p1tchbend.tool@gmail.com>
2025-04-27 02:32:58 +0900

    環境変数保存用クラス追加

---
+++ Env.cs
@@ -0,0 +1,14 @@
+﻿namespace Boostera
+{
+    public class Env
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Env(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
