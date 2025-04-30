d5da9bc874470b8a02114590be739a62d157ea57
p1tchbend-tool <p1tchbend.tool@gmail.com>
2025-05-01 01:48:16 +0900

    バージョン 3.2.2 リリース

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
+[assembly: AssemblyVersion("3.2.2.0")]
+[assembly: AssemblyFileVersion("3.2.2.0")]
